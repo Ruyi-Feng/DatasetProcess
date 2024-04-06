3b633acb83f6d3f92eac23827cec2da3f648dd52
Ruyi-Feng <fengruyi@seu.edu.cn>
2024-04-06 22:56:54 +0800

    fix: application

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -4,17 +4,27 @@ from data_provider.utils import equal_img_labels
 from data_provider.data_fomat_driver import DataConvert
 
 def test_checker():
-    checker = Checker(dir="J:\\yolov5projdataset\\roadside", fmt="yolo2xyxy", if_check_inside_obj=True)
+    checker = Checker(dir="J:\\yolov5projdataset\\roadside", fmt="yolo2xyxy", if_check_inside_obj=True,
+                      json_path="check_result.json"
+                      )
     sample_num = checker.get_sample_number()
     print("sample_num: ", sample_num)
+    # checker.check_samples(if_random=True, rows=3, cols=3, save_path="check_result.json", col_pix=1920, row_pix=1080)
+    # checker.del_error_samples("check_result.json")
     # assert sample_num == 19
 
 def test_balencer():
-    balencer = Balencer(dataset_info_json_path="check_result.json")
-    balencer.balence(tolerant_offset=0.7, expect_dataset_scale=0.999)
+    balencer = Balencer(dataset_info_json_path="check_result.json", generate_top_pool=False)
+    # balencer.load_top_pool()
+    # balencer.balence(tolerant_offset=0.3,
+    #                  expect_dataset_scale=0.30,)
+    balencer.sampling_refer_balenced_log("balence_log.json", "J:\\yolov5projdataset\\afterbalence")
 
 def test_euqal_img_labels():
     equal_img_labels("J:\\yolov5projdataset\\roadside")
 
 def test_change_label_cls():
-    DataConvert.change_label_cls(labels_path="", ori_cls=0, new_cls=1)
+    DataConvert.change_label_cls(labels_path="J:\\yolov5projdataset\\Yolo Truck.v2i.yolov5pytorch\\train\\labels",
+                                 ori_cls=1,
+                                 new_cls=3)
+
