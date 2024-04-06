5bbaf1ea578f75e6a9b10473ca4be545fb2b4395
Ruyi-Feng <fengruyi@seu.edu.cn>
2024-04-06 16:57:39 +0800

    feat: tests api

--- tests.py
+++ tests.py
@@ -1,10 +1,22 @@
-from tests.test_checker import test_checker, test_balencer
-from tests.test_lightchange import test_lightchange, test_cutchange_exteral_label, test_cutchange_internal, test_cutchange_exteral_bbox
+from tests.test_checker import (
+    test_checker,
+    test_balencer,
+    test_euqal_img_labels,
+    test_change_label_cls,
+)
+from tests.test_lightchange import (
+    test_lightchange,
+    test_cutchange_exteral_label,
+    test_cutchange_internal,
+    test_cutchange_exteral_bbox,
+)
 import time
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_time = time.time()
+    test_euqal_img_labels()
+    test_change_label_cls()
     test_checker()
     test_balencer()
     test_lightchange()
@@ -12,6 +24,4 @@ if __name__ == '__main__':
     test_cutchange_exteral_bbox()
     test_cutchange_internal()
     end_time = time.time()
-    print('time:', end_time - start_time)
-
-
+    print("time:", end_time - start_time)
